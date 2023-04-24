6018c2e8e1be48292d92f47c53b9c909b2f8b06c
Alucherdi <hjgo127942@gmail.com>
2023-04-24 01:18:23 -0600

    Adding LspAttach autocommand

--- lua/alucherdi/plugconfs/lsp.lua
+++ lua/alucherdi/plugconfs/lsp.lua
@@ -1,61 +1,62 @@
 local lsp = require('lspconfig')
-
 local util = require('lspconfig/util')
 
-local nbopts = { noremap = true, silent = true }
-vim.keymap.set('n', '<space>d', vim.diagnostic.open_float, nbopts)
-vim.keymap.set('n', '[g', vim.diagnostic.goto_prev, nbopts)
-vim.keymap.set('n', ']g', vim.diagnostic.goto_next, nbopts)
-vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, nbopts)
+vim.keymap.set('n', '<space>d', vim.diagnostic.open_float)
+vim.keymap.set('n', '[g', vim.diagnostic.goto_prev)
+vim.keymap.set('n', ']g', vim.diagnostic.goto_next)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
 
-local on_attach = function(client, bufnr)
-    vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-    client.server_capabilities.semanticTokensProvider = nil
+vim.api.nvim_create_autocmd('LspAttach', {
+    group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+    callback = function(ev)
+        vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
 
-    local tsgd = '<cmd>tab split | lua vim.lsp.buf.definition()<cr>'
-    local vsgd = '<cmd>vsplit | lua vim.lsp.buf.definition()<cr>'
-    local sgd = '<cmd>split | lua vim.lsp.buf.definition()<cr>'
+        local client = vim.lsp.get_client_by_id(ev.data.client_id)
+        client.server_capabilities.semanticTokensProvider = nil
 
-    local bufopts = { noremap=true, silent=true, buffer=bufnr }
-    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-    vim.keymap.set('n', 'gt', tsgd, bufopts)
-    vim.keymap.set('n', 'g_', sgd, bufopts)
-    vim.keymap.set('n', 'g-', vsgd, bufopts)
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-    --vim.keymap.set('n', '<C-s>', vim.lsp.buf.signature_help, bufopts)
-    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
-    vim.keymap.set('n', '<space>wl', function()
-          print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-    end, bufopts)
-    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
-    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
-    vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
-    vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-    vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
-end
+        local tsgd = '<cmd>tab split | lua vim.lsp.buf.definition()<cr>'
+        local vsgd = '<cmd>vsplit | lua vim.lsp.buf.definition()<cr>'
+        local sgd = '<cmd>split | lua vim.lsp.buf.definition()<cr>'
+
+        local opts = { buffer = ev.buf }
+        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+        vim.keymap.set('n', 'gt', tsgd, opts)
+        vim.keymap.set('n', 'g_', sgd, opts)
+        vim.keymap.set('n', 'g-', vsgd, opts)
+        vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+        --vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+        vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+        vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+        vim.keymap.set('n', '<space>wl', function()
+            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+        end, opts)
+        vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+        vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+        vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+        vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+        vim.keymap.set('n', '<space>f', function()
+            vim.lsp.buf.format { async = true }
+        end, opts)
+    end,
+})
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
-lsp.html.setup { on_attach = on_attach }
-lsp.emmet_ls.setup { on_attach = on_attach }
-lsp.cssls.setup {
-    on_attach = on_attach,
-    capabilities = capabilities
-}
+lsp.html.setup {}
+lsp.emmet_ls.setup {}
+lsp.cssls.setup {capabilities = capabilities}
 
-lsp.tsserver.setup { on_attach = on_attach }
-lsp.svelte.setup { on_attach = on_attach }
+lsp.tsserver.setup {}
+lsp.svelte.setup {}
 
-lsp.jdtls.setup { on_attach = on_attach }
+lsp.jdtls.setup {}
 lsp.dartls.setup {
-    on_attach = on_attach,
     root_dir = util.root_pattern("pubspec.yaml", "*.dart", "main.dart")
 }
-lsp.rust_analyzer.setup { on_attach = on_attach }
-lsp.clangd.setup { on_attach = on_attach }
-lsp.cmake.setup { on_attach = on_attach }
+lsp.rust_analyzer.setup {}
+lsp.clangd.setup {}
+lsp.cmake.setup {}
